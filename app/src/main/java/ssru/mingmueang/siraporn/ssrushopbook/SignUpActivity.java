d25702067509c1ea4e4049543c8ebb42aaf1c539
SRP <vaspa_zomo@hotmail.com>
2016-05-30 15:07:27 +0700

    Check Space

--- app/src/main/java/ssru/mingmueang/siraporn/ssrushopbook/SignUpActivity.java
+++ app/src/main/java/ssru/mingmueang/siraporn/ssrushopbook/SignUpActivity.java
@@ -1,13 +1,55 @@
 package ssru.mingmueang.siraporn.ssrushopbook;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static ssru.mingmueang.siraporn.ssrushopbook.R.id.editText3;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private EditText nameEditText, surnameEditText, userEditText , passEditText;
+    private String nameString, surString, userString, passString;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(editText3);
+        passEditText = (EditText) findViewById(R.id.editText4);
+
+
+    } //Main Method
+
+    public void clickSignUpSign(View view){
+
+        nameString = nameEditText.getText().toString().trim();
+        surString = surnameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passString = passEditText.getText().toString().trim();
+
+        //Check Space ตรวจสอบหาความว่างเปล่าของสิ่งที่กรอก
+        if (nameString.equals("") || surString.equals("") ||
+                userString.equals("")|| passString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "ข้อมูลไม่ครบถ้วน", "กรุณากรอกให้ครบถ้วน");
+
+        } else {
+            //no Space
+        }
+
+
+    }   //clickSign
+
+} //Main Class
