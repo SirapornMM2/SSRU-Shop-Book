5b8ca3c3f2981ac0f9a0054626b0aedf4889f4b4
SRP <vaspa_zomo@hotmail.com>
2016-05-31 10:20:03 +0700

    Finish

--- app/src/main/java/ssru/mingmueang/siraporn/ssrushopbook/SignUpActivity.java
+++ app/src/main/java/ssru/mingmueang/siraporn/ssrushopbook/SignUpActivity.java
@@ -1,10 +1,21 @@
 package ssru.mingmueang.siraporn.ssrushopbook;
 
+import android.app.DownloadManager;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 import static ssru.mingmueang.siraporn.ssrushopbook.R.id.editText3;
 
@@ -43,7 +54,7 @@ public class SignUpActivity extends AppCompatActivity {
                 userString.equals("")|| passString.equals("")) {
             //Have Space
             MyAlert myAlert = new MyAlert();
-            myAlert.myDialog(this, "ข้อมูลไม่ครบถ้วน", "กรุณากรอกให้ครบถ้วน");
+            myAlert.myDialog(this, "ข้อมูลไม่ครบถ้วน", "กรุณากรอกข้อมูลให้ครบถ้วน");
 
         } else {
             //no Space
@@ -54,7 +65,31 @@ public class SignUpActivity extends AppCompatActivity {
     }   //clickSign
 
     private void urlUploadNewUser() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd" , "true")
+                .add("Name" , nameString)
+                .add("Surname" , surString)
+                .add("User" , userString)
+                .add("Password" , passString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/ssru/add_user_SRP.php").post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+
+            }
+        });
 
     }   // uplodeNewUser
 
 } //Main Class
+
