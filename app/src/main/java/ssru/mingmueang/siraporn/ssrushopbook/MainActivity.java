8772a6886042c78dd6eb75a7a4945aa655b3de0d
SRP <vaspa_zomo@hotmail.com>
2016-05-31 13:17:15 +0700

    Delete

--- app/src/main/java/ssru/mingmueang/siraporn/ssrushopbook/MainActivity.java
+++ app/src/main/java/ssru/mingmueang/siraporn/ssrushopbook/MainActivity.java
@@ -1,6 +1,7 @@
 package ssru.mingmueang.siraporn.ssrushopbook;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,9 +22,19 @@ public class MainActivity extends AppCompatActivity {
         myManage = new MyManage(MainActivity.this);
 
         //Test add Value to SQLite
-        myManage.addNewUser("new", "sur", "user", "pass", "money");
+       // myManage.addNewUser("new", "sur", "user", "pass", "money");
+
+        //Detele All userTABLE
+        deleteAlluserTABLE();
 
     }   //Main Method
+
+    private void deleteAlluserTABLE() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table,null, null);
+    }   // delete
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
